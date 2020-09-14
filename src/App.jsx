f76507e6743bc0d8b25f4c2eee08529b92d5f33c
Matt Eden <mrmatteden@gmail.com>
2020-09-14 20:14:34 +1200

    Update look from default create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,25 @@ export const App = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>React Template</p>
-                <a
-                    className="App-link"
-                    href="https://github.com/Matteas-Eden/react-template"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    View Source
-                </a>
+                <p>
+                    React Template -{' '}
+                    <a
+                        className="App-link"
+                        href="https://github.com/Matteas-Eden/react-template"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Get it here!
+                    </a>
+                </p>
+                <div style={{ textAlign: 'left' }}>
+                    <ul>
+                        <li>ESLint for styling source files</li>
+                        <li>Prettier for styling the rest</li>
+                        <li>Husky for on-commit linting</li>
+                        <li>GitHub Pages Deployment</li>
+                    </ul>
+                </div>
             </header>
         </div>
     );
